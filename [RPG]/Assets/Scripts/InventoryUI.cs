07f76080703735249d874a0827bb18adf40394dd
robawesome <robduboff@gmail.com>
2017-09-27 07:39:24 -0700

    v0.0001 - First Build Player movement Inventory UI Item PickUp

---
+++ [RPG]/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+
+    public Transform itemsParent;
+
+    Inventory inventory;
+
+    InventoryIcon[] icons;
+
+	// Use this for initialization
+	void Start () {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+
+        icons = itemsParent.GetComponentsInChildren<InventoryIcon>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+
+        for (int i = 0; i < icons.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                icons[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                icons[i].ClearSlot();
+            }
+        }
+    }
+}
